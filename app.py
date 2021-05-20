7f4c687dd158f6df4accae273ea8c8d60950a861
Ray <q8977452@gmail.com>
2021-05-20 14:27:36 +0800

    feat: add 使用說明和附近餐廳

--- app.py
+++ app.py
@@ -17,6 +17,22 @@ line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
 
 
+def sendText():
+    try:
+        message = [
+            TextSendMessage(
+                text="還沒撰寫說明唷!\n 開發者LineId:perplexed"
+            ),
+            StickerSendMessage(
+                package_id='1',
+                sticker_id='2'
+            )
+        ]
+        line_bot_api.reply_message(event.reply_token, message)
+    except:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='發生錯誤！\n聯絡我感恩!'))
+
+
 def sendMovie():
     def get_date(date_str):
         # e.g. "上映日期：2017-03-23" -> match.group(0): "2017-03-23"
@@ -33,6 +49,8 @@ def sendMovie():
     Yahoo_INTRO_URL = 'https://tw.movies.yahoo.com/movieinfo_main.html'  # 詳細資訊
     Yahoo_PHOTO_URL = 'https://tw.movies.yahoo.com/movieinfo_photos.html'  # 劇照
     Yahoo_TIME_URL = 'https://tw.movies.yahoo.com/movietime_result.html'  # 時刻表
+
+    resp = requests.get(Yahoo_MOVIE_URL)
 
     try:
         if resp.status_code != 200:
@@ -130,7 +148,7 @@ def sendMovie():
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text='發生錯誤！\n聯絡我感恩!'))
 
 
-def sendBeautyImg(event):  # 傳送表特版美女或恐龍圖
+def sendBeautyImg():  # 傳送表特版美女或恐龍圖
 
     def get_web_page(url):  # 取得網頁
         resp = requests.get(
@@ -188,6 +206,127 @@ def sendBeautyImg(event):  # 傳送表特版美女或恐龍圖
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text='發生錯誤！\n聯絡我感恩!'))
 
 
+def sendRestaurant(event):  # 傳送附近餐廳
+    # 地址
+    # userAddress = event.message.address
+    # 緯度
+    latitude = event.message.latitude
+    # 經度
+    longitude = event.message.longitude
+
+    # googlePlace_url = "https://www.google.com.tw/maps/place/"
+
+    google_apikey = "&key=" + "你的api"
+
+    Nearby_search_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + str(
+        latitude) + ',' + str(longitude) + " &radius=1500&type=restaurant" + google_apikey
+
+    pictureApi = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=500&photoreference='
+
+    restaurantData = requests.get(Nearby_search_url).json()
+
+    allRestaurantList = list()
+    for item in restaurantData['results'][0:5]:
+        RestaurantList = list()
+        # 名子0
+        RestaurantList.append(item['name'])
+        # 地址1
+        RestaurantList.append(item['vicinity'])
+        # 緯度2
+        RestaurantList.append(str(item['geometry']['location']['lat']))
+        # 經度3
+        RestaurantList.append(str(item['geometry']['location']['lng']))
+        # 評價4
+        RestaurantList.append(str(item['rating']))
+        # 是否營業5
+        RestaurantList.append(str(item['opening_hours']['open_now']))
+        # 總留言數6
+        RestaurantList.append(str(item['user_ratings_total']))
+        # 相片網址7
+        RestaurantList.append(pictureApi + item['photos'][0]['photo_reference'] + google_apikey)
+        allRestaurantList.append(RestaurantList)
+    try:
+        message = TemplateSendMessage(
+            alt_text="餐廳多頁訊息",
+            template=CarouselTemplate(
+                columns=[
+                    CarouselColumn(
+                        thumbnail_image_url=allRestaurantList[0][7],
+                        title="餐廳:" + allRestaurantList[0][0],
+                        text="Google評價:" + allRestaurantList[0][4] + "\n營業中:" + allRestaurantList[0][5] + "\n總評論數" +
+                             allRestaurantList[0][6],
+                        actions=[
+
+                            URITemplateAction(
+                                label='出發',
+                                uri="https://www.google.com/maps/search/?api=1&query=" + allRestaurantList[0][2] + "," +
+                                    allRestaurantList[0][3]
+                            ),
+
+                        ]
+                    ),
+                    CarouselColumn(
+                        thumbnail_image_url=allRestaurantList[1][7],
+                        title="餐廳:" + allRestaurantList[1][0],
+                        text="Google評價:" + allRestaurantList[1][4] + "\n營業中:" + allRestaurantList[1][5] + "\n總評論數" +
+                             allRestaurantList[1][6],
+                        actions=[
+
+                            URITemplateAction(
+                                label='出發',
+                                uri="https://www.google.com/maps/search/?api=1&query=" + allRestaurantList[1][2] + "," +
+                                    allRestaurantList[1][3]
+                            ),
+
+                        ]
+                    ),
+                    CarouselColumn(
+                        thumbnail_image_url=allRestaurantList[2][7],
+                        title="餐廳:" + allRestaurantList[2][0],
+                        text="Google評價:" + allRestaurantList[2][4] + "\n營業中:" + allRestaurantList[2][5] + "\n總評論數" +
+                             allRestaurantList[2][6],
+                        actions=[
+                            URITemplateAction(
+                                label='出發',
+                                uri="https://www.google.com/maps/search/?api=1&query=" + allRestaurantList[2][2] + "," +
+                                    allRestaurantList[2][3]
+                            )
+                        ]
+                    ),
+                    CarouselColumn(
+                        thumbnail_image_url=allRestaurantList[3][7],
+                        title="餐廳:" + allRestaurantList[3][0],
+                        text="Google評價:" + allRestaurantList[3][4] + "\n營業中:" + allRestaurantList[3][5] + "\n總評論數" +
+                             allRestaurantList[3][6],
+                        actions=[
+                            URITemplateAction(
+                                label='出發',
+                                uri="https://www.google.com/maps/search/?api=1&query=" + allRestaurantList[3][2] + "," +
+                                    allRestaurantList[3][3]
+                            )
+                        ]
+                    ),
+                    CarouselColumn(
+                        thumbnail_image_url=allRestaurantList[4][7],
+                        title="餐廳:" + allRestaurantList[4][0],
+                        text="Google評價:" + allRestaurantList[4][4] + "\n營業中:" + allRestaurantList[4][5] + "\n總評論數" +
+                             allRestaurantList[4][6],
+                        actions=[
+                            URITemplateAction(
+                                label='出發',
+                                uri="https://www.google.com/maps/search/?api=1&query=" + allRestaurantList[4][2] + "," +
+                                    allRestaurantList[4][3]
+                            )
+                        ]
+                    )
+                ]
+            )
+        )
+        line_bot_api.reply_message(event.reply_token, message)
+    except:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='挖勒發生錯誤！\n聯絡我感恩!'))
+
+
 @app.route("/", methods=["GET", "POST"])
 def callback():
     if request.method == "GET":
@@ -205,10 +344,20 @@ def callback():
 
 
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
+def handle_message_text(event):
     get_message = event.message.text
-    if get_message == "最新電影":
+    if get_message == "@使用說明":
+        sendText()
+    elif get_message == "@最新電影":
         sendMovie()
+    elif get_message == "@抽":
+        sendBeautyImg()
     else:
         reply = TextSendMessage(text=f"{get_message}")
         line_bot_api.reply_message(event.reply_token, reply)
+
+
+## 沒有google api><"
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message_text(event):
+    sendRestaurant(event)
